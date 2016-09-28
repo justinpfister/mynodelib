1d9fe15b09fe4209a5df0d6fe40543e8c65adc97
justinpfister <jpfister@gmail.com>
2016-09-28 16:27:13 -0400

    First Commit

---
+++ numtest.js
@@ -0,0 +1,23 @@
+
+const numeral = require('numeral');
+const isNumeric = require("isnumeric");
+
+const convertNumber = (num) => {
+  const number = numeral().unformat(num);
+  const lastchar = num.substr(-1);
+  if(!isNumeric(lastchar)){
+      switch(lastchar){
+        case 'B':
+        return number * 1000000000;
+        case 'M':
+        return number * 1000000;
+        case 'K':
+        return number * 1000;
+      }
+  }
+
+  return number;
+
+};
+
+console.log(convertNumber('6.5B'));
